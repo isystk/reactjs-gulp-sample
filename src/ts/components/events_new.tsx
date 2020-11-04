e9de64eeba8bb962fea2bd7b0f3b78fa02f86bdc
iseyoshitaka <iseyoshitaka@team-lab.com>
2020-11-04 15:21:54 +0900

    新規登録画面の入力チェックを追加

--- src/ts/components/events_new.tsx
+++ src/ts/components/events_new.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { connect, MapStateToProps, MapDispatchToProps } from 'react-redux'
+import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom'
 
 // ↓ 表示用のデータ型
@@ -19,16 +20,49 @@ interface appdispatchproperties
 
 export class EventsNew extends React.Component
 {
+
+    renderField(field) {
+        const { input, label, type, meta: { touched, error } } = field
+        return (
+            <div>
+                <input {...input} placeholder={label} type={type} />
+                {touched && error && <span>{error}</span>}
+            </div>
+        )
+    }
+
     render() {
         return (
             <React.Fragment>
-                <div>foo</div>
+                <form>
+                    <div>
+                        <Field label="Title" name="title" type="text" component={this.renderField} />
+                    </div>
+                    <div>
+                        <Field label="Body" name="body" type="text" component={this.renderField} />
+                    </div>
+                    <div>
+                        <input type="submit" value="Submit" disabled={false} />
+                        <Link to="/" >Cancel</Link>
+                    </div>
+                </form>
             </React.Fragment>
         )
     }
 }
 
+const validate = values => {
+    const errors = {
+        title: '',
+        body: ''
+    }
+    if (!values.title) errors.title = 'Titleを入力して下さい'
+    if (!values.body) errors.body = 'Bodyを入力して下さい'
+    return errors
+}
 
 //const mapDispatchToProps = ({ readEvents });
 
-export default connect(null,null)(EventsNew);
+export default connect(null,null)(
+    reduxForm({ validate, form: 'eventNewForm'})(EventsNew)
+)
